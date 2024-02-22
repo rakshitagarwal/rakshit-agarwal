a71abca5ca7783d0d2ca50954fa7ca56d7d37c05
rakshit agarwal <rakshitag13@gmail.com>
2024-02-22 23:49:37 +0530

    deploy ready

---
+++ constants/data.js
@@ -0,0 +1,116 @@
+const userData = {
+  githubUsername: "rakshitagarwal",
+  name: "Rakshit Agarwal",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpeg",
+  email: "rakshitag13@gmail.com",
+  phone: "+91 9997443874",
+  address: "Moradabad, Uttar Pradesh, India.",
+  projects: [
+    {
+      title: "Sales Dashboard",
+      link: "https://dash-client-996c.onrender.com/",
+      imgUrl: "/dashboard.png",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `Rakshit Agarwal brings a wealth of experience as a Software Engineer and IT professional, having dedicated 5 years to mastering his craft in diverse industries such as IT, Exports, and Startups. His track record includes spearheading innovative solutions across 4 full-stack development projects and playing a pivotal role in the growth of a dynamic startup over a span of 2 years. Noteworthy is his adeptness in fostering collaborations with industry leaders like Capgemini, reflecting his ability to thrive in high-pressure environments while delivering top-notch results.`,
+      `With a solid educational foundation in computer science and engineering, Rakshit further fortified his expertise with certifications in Marketing and Operations from IIM Bangalore. His skill set encompasses a comprehensive range of proficiencies, including full-stack web development, backend specialization using Node.js and Express, frontend mastery with React and Next.js, robust REST API design, intricate system architecture, proficient database management utilizing MongoDB and SQL, and excellent proficiency in MS Office tools alongside effective communication skills. Rakshit's multidimensional skill set and hands-on experience position him as a valuable asset capable of driving success in any IT endeavor.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "Globalvox",
+      year: "2023",
+      companyLink: "https://www.globalvoxinc.com/",
+      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+    },
+    {
+      title: "Fullstack Developer",
+      company: "C.L.G. Pvt Ltd",
+      year: "2020",
+      companyLink: "https://clgupta.com/",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Training",
+      company: "Capgemini IT MNC",
+      year: "2019",
+      companyLink: "https://www.capgemini.com/",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Bachelor's Degree",
+      company: "ABES Engineering College, Ghaziabad.",
+      year: "2019",
+      companyLink: "https://abes.ac.in/",
+      desc: "B.Tech in Computer Science and Engineering with First Division. Nobody asks this but it's okay.",
+    },
+    {
+      title: "CBSE Class 12",
+      company: "K.C.M. School, Moradabad.",
+      year: "2015",
+      companyLink: "https://www.kcmmbd.com/",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+    {
+      title: "CBSE Class 10",
+      company: "K.C.M. School, Moradabad.",
+      year: "2013",
+      companyLink: "https://www.kcmmbd.com/",
+      desc: "CBSE syllabus with CCE exams. CGPA: 9.6/10",
+    }
+  ],
+  resumeUrl: "https://drive.google.com/file/d/18ipXcTi_-GFAtT5tBKdbw7657ZIWER0j/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/rakshit__agarwal",
+    twitter: "https://twitter.com/RakshitAgarwal3",
+    linkedin: "https://linkedin.com/in/rakshitagarwal",
+    github: "https://github.com/rakshitagarwal",
+    facebook: "https://www.facebook.com/ag.rakshit",
+    Whatsapp: "https://wa.me/+919997443874",
+  },
+};
+
+export default userData;
